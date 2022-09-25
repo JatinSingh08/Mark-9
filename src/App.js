944ad581b403244c5346a02248a4490819d00383
JatinSingh08 <singhjatin0812@gmail.com>
2022-09-26 04:06:53 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import './App.css';
 import { useState } from 'react';
 const movieList = {
+
+  "Comedy": [
+    {
+      Name: "Dhamaal",
+      image: "https://wallpapercave.com/w/wp7805336.jpg",
+      youtube: "https://youtu.be/6xdRuJoEMoU"
+    },
+    {
+      Name: "Bhool Bhulaiyaa",
+      image: "https://wallpaperaccess.com/full/8264684.jpg",
+      youtube: "hhttps://youtu.be/jzYxbnHHhY4"
+    },
+    {
+      Name: "3 Idiots",
+      image: "https://wallpaperaccess.com/full/3574479.jpg",
+      youtube: "https://youtu.be/K0eDlFX9GMc"
+    }
+  ],
   
   "Romantic": [
     {
       Name: "Student Of the year",
-      image: "/images/soty.jpg",
+      image: "https://wallpapercave.com/wp/wp8202310.jpg",
       youtube: "https://youtu.be/tQ0mzXRk-oM"
     },
     {
       Name: "Yeh Jawaani Hai Deewani",
-      image: "/images/yjhd.jpg",
+      image: "https://wallpapercave.com/wp/wp6754719.jpg",
       youtube: "https://youtu.be/Rbp2XUSeUNE"
     },
     {
       Name: "Ae Dil Hai Mushkil",
-      image: "/images/adhm.jpg",
+      image: "https://wallpapercave.com/wp/wp6584723.jpg",
       youtube: "https://youtu.be/Z_PODraXg4E"
     }
   ],
@@ -23,54 +41,38 @@ const movieList = {
   "Action": [
     {
       Name: "War",
-      image: "/images/war-image.jpg",
+      image: "https://wallpaperaccess.com/full/2330954.jpg",
       youtube: "https://youtu.be/tQ0mzXRk-oM"
     },
     {
       Name: "Don 2",
-      image: "/images/don-2.jpg",
+      image: "https://wallpapercave.com/wp/wp2864025.jpg",
       youtube: "https://youtu.be/_X5w-6PqoZ0"
     },
     {
       Name: "Agent Vinod",
-      image: "/images/agent-vinod.jpg",
+      image: "https://stat1.bollywoodhungama.in/wp-content/uploads/2016/03/51193699.jpg",
       youtube: "https://youtu.be/g7pbAIqUR4M"
     }
   ],
 
 
-  "Comedy": [
-    {
-      Name: "Dhamaal",
-      image: "/images/dhamaal.jpg",
-      youtube: "https://youtu.be/6xdRuJoEMoU"
-    },
-    {
-      Name: "Bhool Bhulaiyaa",
-      image: "/images/bool-bhulaiyaa.jpg",
-      youtube: "hhttps://youtu.be/jzYxbnHHhY4"
-    },
-    {
-      Name: "3 Idiots",
-      image: "/images/3-Idiots.jpg",
-      youtube: "https://youtu.be/K0eDlFX9GMc"
-    }
-  ],
+  
 
   "Thriller": [
     {
       Name: "Drishyam",
-      image: "/images/drishyam.jpg",
+      image: "https://i.pinimg.com/236x/96/df/27/96df272c04f944b7d90eb92bf9731d3b.jpg",
       youtube: "https://youtu.be/AuuX2j14NBg"
     },
     {
       Name: "NH 10",
-      image: "/images/nh-10.jpg",
+      image: "https://www.filmibeat.com/ph-big/2015/02/_14231218990.jpg",
       youtube: "https://youtu.be/A9icNqWlylw"
     },
     {
       Name: "Talvar",
-      image: "/images/talvar.jpg",
+      image: "https://wallpapercave.com/wp/wp6987085.jpg",
       youtube: "https://youtu.be/aQNMsw8Ljjc"
     }
   ]
@@ -78,10 +80,10 @@ const movieList = {
 }
 function App() {
 
-  const [movieName, setMovieNmae] = useState('Action');
+  const [movieName, setMovieName] = useState('Comedy');
 
   function genreClickHandler(genre) {
-    setMovieNmae(genre);
+    setMovieName(genre);
   }
   return (
     <div className="App">
@@ -99,8 +101,8 @@ function App() {
 
         <main className='container'>
           {movieList[movieName].map(movie => 
-            <a className='movie-info' href={movie.youtube} key={movie.number} target="_blank" >
-              <img src={movie.image} alt={movie.Name} className='movie-image'/>
+            <a className='movie-info' href={movie.youtube} key={movie.number} target="_blank" rel="noreferrer">
+              <img src={movie.image} alt={movie.Name} className='movie-image' />
               <span className='movie-name'>{movie.Name}</span>
             </a>
           )}
