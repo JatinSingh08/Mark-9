725592e26996f60738e175e29f6aa45ecc24ee5d
JatinSingh08 <singhjatin0812@gmail.com>
2022-09-28 13:48:36 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -6,17 +6,20 @@ const movieList = {
     {
       Name: "Dhamaal",
       image: "https://wallpapercave.com/w/wp7805336.jpg",
-      youtube: "https://youtu.be/6xdRuJoEMoU"
+      youtube: "https://youtu.be/6xdRuJoEMoU",
+      rating: "4/5"
     },
     {
       Name: "Bhool Bhulaiyaa",
       image: "https://wallpaperaccess.com/full/8264684.jpg",
-      youtube: "hhttps://youtu.be/jzYxbnHHhY4"
+      youtube: "hhttps://youtu.be/jzYxbnHHhY4",
+      rating: "3/5"
     },
     {
       Name: "3 Idiots",
       image: "https://wallpaperaccess.com/full/3574479.jpg",
-      youtube: "https://youtu.be/K0eDlFX9GMc"
+      youtube: "https://youtu.be/K0eDlFX9GMc",
+      rating: "5/5"
     }
   ],
   
@@ -24,17 +27,20 @@ const movieList = {
     {
       Name: "Student Of the year",
       image: "https://wallpapercave.com/wp/wp8202310.jpg",
-      youtube: "https://youtu.be/tQ0mzXRk-oM"
+      youtube: "https://youtu.be/tQ0mzXRk-oM",
+      rating: "2/5"
     },
     {
       Name: "Yeh Jawaani Hai Deewani",
       image: "https://wallpapercave.com/wp/wp6754719.jpg",
-      youtube: "https://youtu.be/Rbp2XUSeUNE"
+      youtube: "https://youtu.be/Rbp2XUSeUNE",
+      rating: "3/5"
     },
     {
       Name: "Ae Dil Hai Mushkil",
       image: "https://wallpapercave.com/wp/wp6584723.jpg",
-      youtube: "https://youtu.be/Z_PODraXg4E"
+      youtube: "https://youtu.be/Z_PODraXg4E",
+      rating: "2/5"
     }
   ],
 
@@ -42,17 +48,20 @@ const movieList = {
     {
       Name: "War",
       image: "https://wallpaperaccess.com/full/2330954.jpg",
-      youtube: "https://youtu.be/tQ0mzXRk-oM"
+      youtube: "https://youtu.be/tQ0mzXRk-oM",
+      rating: "3/5"
     },
     {
       Name: "Don 2",
       image: "https://wallpapercave.com/wp/wp2864025.jpg",
-      youtube: "https://youtu.be/_X5w-6PqoZ0"
+      youtube: "https://youtu.be/_X5w-6PqoZ0",
+      rating: "3/5"
     },
     {
       Name: "Agent Vinod",
       image: "https://stat1.bollywoodhungama.in/wp-content/uploads/2016/03/51193699.jpg",
-      youtube: "https://youtu.be/g7pbAIqUR4M"
+      youtube: "https://youtu.be/g7pbAIqUR4M",
+      rating: "3/5"
     }
   ],
 
@@ -63,17 +72,20 @@ const movieList = {
     {
       Name: "Drishyam",
       image: "https://i.pinimg.com/236x/96/df/27/96df272c04f944b7d90eb92bf9731d3b.jpg",
-      youtube: "https://youtu.be/AuuX2j14NBg"
+      youtube: "https://youtu.be/AuuX2j14NBg",
+      rating: "4/5"
     },
     {
       Name: "NH 10",
       image: "https://www.filmibeat.com/ph-big/2015/02/_14231218990.jpg",
-      youtube: "https://youtu.be/A9icNqWlylw"
+      youtube: "https://youtu.be/A9icNqWlylw",
+      rating: "4/5"
     },
     {
       Name: "Talvar",
       image: "https://wallpapercave.com/wp/wp6987085.jpg",
-      youtube: "https://youtu.be/aQNMsw8Ljjc"
+      youtube: "https://youtu.be/aQNMsw8Ljjc",
+      rating: "3/5"
     }
   ]
 
@@ -103,7 +115,8 @@ function App() {
           {movieList[movieName].map(movie => 
             <a className='movie-info' href={movie.youtube} key={movie.number} target="_blank" rel="noreferrer">
               <img src={movie.image} alt={movie.Name} className='movie-image' />
-              <span className='movie-name'>{movie.Name}</span>
+              <span className='movie-name' style={{fontWeight: 'bold', color: 'wheat'}}>{movie.Name}</span>
+              <span className='movie-rating' style={{color: 'black', fontWeight: 'bold'}}>{movie.rating}</span>
             </a>
           )}
         </main>
